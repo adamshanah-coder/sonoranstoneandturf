b87cf9760a2dd29f3f1a409a12eee614aaa3d6fc
adamshanah-coder <adam.shanah@gmail.com>
2026-06-19 12:47:07 -0700

    Delete components directory

--- components/ui/Button.jsx
+++
@@ -1,3 +0,0 @@
-export default function Button({ href = '#contact', children, variant = 'gold' }) {
-  return <a className={`button ${variant}`} href={href}><span>{children}</span><span>→</span></a>;
-}
